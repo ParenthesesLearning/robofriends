dbbbda2d25e4085bf2ae43c56df91c3a25758f79
Pre-Dev <deveshkorde@users.noreply.github.com>
2020-08-27 18:55:21 +0530

    Updates

---
+++ precache-manifest.2c890b90d33a824964bfd6c2beeb90a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edb2f6456a0d99ed62bc37e432478805",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "a337cf8e7dc4e64b2f05",
+    "url": "/robofriends/static/css/main.1f1dcaf5.chunk.css"
+  },
+  {
+    "revision": "2f33704f441e1b3ca17f",
+    "url": "/robofriends/static/js/2.27061b76.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.27061b76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a337cf8e7dc4e64b2f05",
+    "url": "/robofriends/static/js/main.28287963.chunk.js"
+  },
+  {
+    "revision": "623de5eb0aa8418ede38",
+    "url": "/robofriends/static/js/runtime-main.c0d7f375.js"
+  }
+]);
